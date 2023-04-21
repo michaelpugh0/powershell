dcb781eba84e59dcd3a760784844036075058368
michaelpugh0 <131415498+michaelpugh0@users.noreply.github.com>
2023-04-21 01:48:28 +0100

    Update scheduledreboot.ps1
    1.4

--- scheduledreboot.ps1
+++ scheduledreboot.ps1
@@ -11,7 +11,7 @@ $Trigger = New-ScheduledTaskTrigger -Once -At $RebootDateTime
 
 try {
     Register-ScheduledTask -TaskName $TaskName -Action $Action -Trigger $Trigger -Settings (New-ScheduledTaskSettingsSet) -User "System"
-    Write-EventLog -LogName System -Source "PowerShell" -EventId 69 -Message "Reboot date: $RebootDate`r`nReboot Time: $RebootTime`r`nTicket Number: $TicketNumber`r`n`r`nExecuted by '$env:USERNAME' at '$([DateTime]::UtcNow.ToString('ddd, dd MMM yyyy HH:mm:ss GMT'))'" -ErrorAction Stop
+    Write-EventLog -LogName System -Source "Windows PowerShell" -EventId 69 -Message "Reboot date: $RebootDate`r`nReboot Time: $RebootTime`r`nTicket Number: $TicketNumber`r`n`r`nExecuted by '$env:USERNAME' at '$([DateTime]::UtcNow.ToString('ddd, dd MMM yyyy HH:mm:ss GMT'))'" -ErrorAction Stop
     Write-Host "Task '$TaskName' has been scheduled successfully for $RebootDate at $RebootTime." -ForegroundColor Green
 } catch {
     Write-EventLog -LogName System -Source "PowerShell" -EventId 69 -Message "Error creating task '$TaskName': $($_.Exception.Message)`r`n`r`nExecuted by '$env:USERNAME' at '$([DateTime]::UtcNow.ToString('ddd, dd MMM yyyy HH:mm:ss GMT'))'" -ErrorAction Stop
