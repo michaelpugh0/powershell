4f1ac01ef4e8153434b06d0f8cff9acfd4689706
michaelpugh0 <131415498+michaelpugh0@users.noreply.github.com>
2023-04-21 01:30:13 +0100

    Add files via upload
    v1

---
+++ scheduledreboot.ps1
@@ -0,0 +1,23 @@
+﻿Set-ExecutionPolicy Unrestricted
+
+$rebootDate = Read-Host "Enter reboot date in DD/MM/YY format"
+$rebootTime = Read-Host "Enter reboot time in HH:mm format"
+$ticketNumber = Read-Host "Enter ticket number"
+
+$rebootDateTime = "$rebootDate $rebootTime"
+$taskName = "SysGroup One Time Reboot $ticketNumber"
+$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-Command Restart-Computer -Force"
+$trigger = New-ScheduledTaskTrigger -Once -At $rebootDateTime
+
+try {
+    Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings (New-ScheduledTaskSettingsSet) -User "System" -ErrorAction SilentlyContinue
+    Write-EventLog -LogName System -Source "PowerShell" -EventId 69 -Message "Reboot date: $rebootDate`r`nReboot Time: $rebootTime`r`nTicket Number: $ticketNumber`r`n`r`nExecuted by '$env:USERNAME' at '$([DateTime]::UtcNow.ToString('ddd, dd MMM yyyy HH:mm:ss GMT'))'" -ErrorAction SilentlyContinue
+    Write-Host "Task '$taskName' has been scheduled successfully for $rebootDate at $rebootTime." -ForegroundColor Green
+} catch {
+    Write-EventLog -LogName System -Source "PowerShell" -EventId 69 -Message "Error creating task '$taskName': $($_.Exception.Message)`r`n`r`nExecuted by '$env:USERNAME' at '$([DateTime]::UtcNow.ToString('ddd, dd MMM yyyy HH:mm:ss GMT'))'" -ErrorAction SilentlyContinue
+    if ($env:USERNAME -ne "Michael.Pugh") {
+        Write-Host "Task creation failed. Please check the event log for more information." -ForegroundColor Red
+    } else {
+        throw
+    }
+}
