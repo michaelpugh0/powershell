ff5d5d5376d07a984aad539b4ac0587b9ad77642
michaelpugh0 <131415498+michaelpugh0@users.noreply.github.com>
2023-04-21 21:47:39 +0100

    Update SQLLogFileCleanup.ps1

--- SQLLogFileCleanup.ps1
+++ SQLLogFileCleanup.ps1
@@ -1,7 +1,9 @@
-# Deletes any SQL Log file in D:/ older than 30 days
+# Deletes any SQL Log file on target drive letter older than 30 days
+# Prompt the user to enter the drive letter where SQL log files are stored
+$driveLetter = Read-Host "Enter the drive letter where SQL log files are stored (e.g. D:)"
 
 # Set the path to the SQL log folder
-$logFolder = "D:\MSSQL\Log"
+$logFolder = "$driveLetter\MSSQL\Log"
 
 # Get the current date and subtract 30 days
 $cutOffDate = (Get-Date).AddDays(-30)
@@ -9,11 +11,27 @@ $cutOffDate = (Get-Date).AddDays(-30)
 # Get all SQL log files that are older than 30 days
 $oldLogFiles = Get-ChildItem $logFolder -Filter "*.log" -Recurse | Where-Object { $_.LastWriteTime -lt $cutOffDate }
 
+# Check if any old log files were found
+if ($oldLogFiles.Count -eq 0) {
+    Write-Host "No SQL log files older than 30 days were found."
+    Exit
+}
+
+# Explain what the script will do and ask for confirmation before proceeding
+$numFilesToDelete = $oldLogFiles.Count
+$sizeToDelete = ($oldLogFiles | Measure-Object -Property Length -Sum).Sum / 1MB
+$confirmationMessage = "This will delete $numFilesToDelete SQL log files totaling $sizeToDelete MB that are older than 30 days from $logFolder. Do you want to proceed? (Y/N)"
+$userConfirmation = Read-Host $confirmationMessage
+
+if ($userConfirmation.ToLower() -ne "y") {
+    Write-Host "Cleanup aborted by user."
+    Exit
+}
+
 # Delete each old log file
 foreach ($oldLogFile in $oldLogFiles) {
     Remove-Item $oldLogFile.FullName -Force
 }
 
 # Output the number of files deleted
-$numFilesDeleted = $oldLogFiles.Count
-Write-Host "$numFilesDeleted old SQL log files have been deleted."
+Write-Host "$numFilesToDelete old SQL log files totaling $sizeToDelete MB have been deleted from $logFolder."
