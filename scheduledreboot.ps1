0101030d5c1f17c5e3c0220d64b4019e04a3ac99
michaelpugh0 <131415498+michaelpugh0@users.noreply.github.com>
2023-04-21 01:55:27 +0100

    Update scheduledreboot.ps1
    1.6

--- scheduledreboot.ps1
+++ scheduledreboot.ps1
@@ -12,7 +12,7 @@ try {
     Write-EventLog -LogName System -Source "PowerShell" -EventId 69 -Message "Reboot date: $rebootDate`r`nReboot Time: $rebootTime`r`nTicket Number: $ticketNumber`r`n`r`nExecuted by '$env:USERNAME' at '$([DateTime]::UtcNow.ToString('ddd, dd MMM yyyy HH:mm:ss GMT'))'"
     Write-Host -ForegroundColor Green "Task '$taskName' has been scheduled successfully for $rebootDate at $rebootTime."
 } catch {
-    Write-EventLog -LogName System -Source "PowerShell" -EventId 69 -Message "Error creating task '$taskName': $($_.Exception.Message)`r`n`r`nExecuted by '$env:USERNAME' at '$([DateTime]::UtcNow.ToString('ddd, dd MMM yyyy HH:mm:ss GMT'))'"
+   Write-EventLog -LogName "Windows PowerShell" -Source "PowerShell" -EventId 69 -Message "Reboot date: $rebootDate`r`nReboot Time: $rebootTime`r`nTicket Number: $ticketNumber`r`n`r`nExecuted by '$env:USERNAME' at '$([DateTime]::UtcNow.ToString('ddd, dd MMM yyyy HH:mm:ss GMT'))'"
     Write-Host -ForegroundColor Red "Task creation failed. Please check the event log for more information."
 }
 
