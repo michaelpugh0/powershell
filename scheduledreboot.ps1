0892918c42e3567dd55da17cca1eabb36ec7abc4
michaelpugh0 <131415498+michaelpugh0@users.noreply.github.com>
2023-04-24 16:04:43 +0100

    Update scheduledreboot.ps1

--- scheduledreboot.ps1
+++ scheduledreboot.ps1
@@ -11,15 +11,16 @@ $ticketNumber = Read-Host -Prompt "Ticket Number"
 
 try {
     # Create new scheduled task
+    $taskName = "SysGroup One time reboot $ticketNumber"
     $action = New-ScheduledTaskAction -Execute "shutdown.exe" -Argument "/r /t 60 /f"
     $trigger = New-ScheduledTaskTrigger -Once -At $rebootDateTime
-    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "SysGroup One time reboot" -Description "Reboots the computer at $rebootDateTime for ticket $ticketNumber" -User "SYSTEM" -RunLevel Highest
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description "Reboots the computer at $rebootDateTime for ticket $ticketNumber" -User "SYSTEM" -RunLevel Highest
 
     # Write event to Windows Event Log
     $eventMessage = "Reboot scheduled for $rebootDateTime for ticket $ticketNumber."
     $eventLog = New-Object System.Diagnostics.EventLog("System")
     $eventLog.Source = "PowerShell Scheduled Reboot"
-    $eventLog.WriteEntry($eventMessage, [System.Diagnostics.EventLogEntryType]::Information)
+    $eventLog.WriteEntry($eventMessage, [System.Diagnostics.EventLogEntryType]::Information, 69)
 
     Write-Host $eventMessage
 }
