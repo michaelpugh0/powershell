680759b7111e083a62b2fc87e8eef559da771cde
michaelpugh0 <131415498+michaelpugh0@users.noreply.github.com>
2023-04-24 15:31:42 +0100

    Update scheduledreboot.ps1

--- scheduledreboot.ps1
+++ scheduledreboot.ps1
@@ -1,19 +1,20 @@
-$rebootDate = Read-Host "Enter reboot date in DD/MM/YY format"
-$rebootTime = Read-Host "Enter reboot time in HH:mm format"
-$ticketNumber = Read-Host "Enter ticket number"
+# Prompt user for reboot date and time
+$rebootDate = Read-Host -Prompt "Date for reboot (DD/MM/YY)"
+$rebootTime = Read-Host -Prompt "Time for reboot (HH:mm)"
+$rebootDateTime = [DateTime]::ParseExact("$rebootDate $rebootTime", "dd/MM/yy HH:mm", $null)
 
-$rebootDateTime = "$rebootDate $rebootTime"
-$taskName = "SysGroup One Time Reboot $ticketNumber"
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-Command Restart-Computer -Force"
+# Prompt user for ticket number
+$ticketNumber = Read-Host -Prompt "Ticket Number"
+
+# Create new scheduled task
+$action = New-ScheduledTaskAction -Execute "shutdown.exe" -Argument "/r /t 60 /f"
 $trigger = New-ScheduledTaskTrigger -Once -At $rebootDateTime
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Reboot" -Description "Reboots the computer at $rebootDateTime for ticket $ticketNumber" -User "SYSTEM" -RunLevel Highest
 
-try {
-    Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings (New-ScheduledTaskSettingsSet) -User "System"
-    Write-EventLog -LogName System -Source "PowerShell" -EventId 69 -Message "Reboot date: $rebootDate`r`nReboot Time: $rebootTime`r`nTicket Number: $ticketNumber`r`n`r`nExecuted by '$env:USERNAME' at '$([DateTime]::UtcNow.ToString('ddd, dd MMM yyyy HH:mm:ss GMT'))'" -ErrorAction SilentlyContinue
-    Write-Host -ForegroundColor Green "Task '$taskName' has been scheduled successfully for $rebootDate at $rebootTime."
-} catch {
-    Write-EventLog -LogName System -Source "PowerShell" -EventId 69 -Message "Error creating task '$taskName': $($_.Exception.Message)`r`n`r`nExecuted by '$env:USERNAME' at '$([DateTime]::UtcNow.ToString('ddd, dd MMM yyyy HH:mm:ss GMT'))'" -ErrorAction SilentlyContinue
-    Write-Host -ForegroundColor Red "Task creation failed. Please check the event log for more information."
-}
+# Write event to Windows Event Log
+$eventMessage = "Reboot scheduled for $rebootDateTime for ticket $ticketNumber."
+$eventLog = New-Object System.Diagnostics.EventLog("System")
+$eventLog.Source = "PowerShell Scheduled Reboot"
+$eventLog.WriteEntry($eventMessage, [System.Diagnostics.EventLogEntryType]::Information)
 
-
+Write-Host $eventMessage
