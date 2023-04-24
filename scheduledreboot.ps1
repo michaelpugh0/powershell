ce5515b9ccaf8a70d67c04b5a6d93f873fd54e58
michaelpugh0 <131415498+michaelpugh0@users.noreply.github.com>
2023-04-24 16:01:46 +0100

    Update scheduledreboot.ps1

--- scheduledreboot.ps1
+++ scheduledreboot.ps1
@@ -9,15 +9,21 @@ $rebootDateTime = [DateTime]::ParseExact("$rebootDate $rebootTime", "dd/MM/yy HH
 # Prompt user for ticket number
 $ticketNumber = Read-Host -Prompt "Ticket Number"
 
-# Create new scheduled task
-$action = New-ScheduledTaskAction -Execute "shutdown.exe" -Argument "/r /t 60 /f"
-$trigger = New-ScheduledTaskTrigger -Once -At $rebootDateTime
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Reboot" -Description "Reboots the computer at $rebootDateTime for ticket $ticketNumber" -User "SYSTEM" -RunLevel Highest
+try {
+    # Create new scheduled task
+    $action = New-ScheduledTaskAction -Execute "shutdown.exe" -Argument "/r /t 60 /f"
+    $trigger = New-ScheduledTaskTrigger -Once -At $rebootDateTime
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "SysGroup One time reboot" -Description "Reboots the computer at $rebootDateTime for ticket $ticketNumber" -User "SYSTEM" -RunLevel Highest
 
-# Write event to Windows Event Log
-$eventMessage = "Reboot scheduled for $rebootDateTime for ticket $ticketNumber."
-$eventLog = New-Object System.Diagnostics.EventLog("System")
-$eventLog.Source = "PowerShell Scheduled Reboot"
-$eventLog.WriteEntry($eventMessage, [System.Diagnostics.EventLogEntryType]::Information)
+    # Write event to Windows Event Log
+    $eventMessage = "Reboot scheduled for $rebootDateTime for ticket $ticketNumber."
+    $eventLog = New-Object System.Diagnostics.EventLog("System")
+    $eventLog.Source = "PowerShell Scheduled Reboot"
+    $eventLog.WriteEntry($eventMessage, [System.Diagnostics.EventLogEntryType]::Information)
 
-Write-Host $eventMessage
+    Write-Host $eventMessage
+}
+catch {
+    Write-Warning "An error occurred while scheduling the reboot. Please contact Michael.Pugh@SysGroup.com."
+    Write-Error $_.Exception.Message
+}
